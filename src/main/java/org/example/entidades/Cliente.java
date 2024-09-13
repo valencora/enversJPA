e38c1f2ed38b9b84f8271218a86c6e3f0acca7ac
valencora <coratolovalen@gmail.com>
2024-09-12 21:56:22 -0300

    envers

---
+++ src/main/java/org/example/entidades/Cliente.java
@@ -0,0 +1,40 @@
+package org.example.entidades;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Audited
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
+    private Domicilio domicilio;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "fk_cliente")
+    @Builder.Default
+    @NotAudited
+    private List<Factura> facturas = new ArrayList<>();
+
+}
